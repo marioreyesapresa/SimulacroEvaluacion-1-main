8d8c2e0819714d819d749ebb6d3331be8febc783
Mario Reyes <mreyesapresa@gmail.com>
2023-10-28 12:06:22 +0200

    first commit

---
+++ DeliverUS-Backend/controllers/validation/RestaurantValidation.js
@@ -0,0 +1,84 @@
+const { check } = require('express-validator')
+const { checkFileIsImage, checkFileMaxSize } = require('./FileValidationHelper')
+const maxFileSize = 2000000 // around 2Mb
+const models = require('../../models')
+const Restaurant = models.Restaurant
+
+// solucion
+const checkBusinessRuleOneResturantPromotedByOwner = async (ownerId, promotedValue) => {
+  if (promotedValue) {
+    try {
+      const promotedRestaurants = await Restaurant.findAll({ where: { userId: ownerId, promoted: true } })
+      if (promotedRestaurants.length !== 0) {
+        return Promise.reject(new Error('You can only promote one restaurant at a time'))
+      }
+    } catch (err) {
+      return Promise.reject(new Error(err))
+    }
+  }
+  return Promise.resolve('ok')
+}
+module.exports = {
+  create: [
+    check('name').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+    check('description').optional({ nullable: true, checkFalsy: true }).isString().trim(),
+    check('address').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+    check('postalCode').exists().isString().isLength({ min: 1, max: 255 }),
+    check('url').optional({ nullable: true, checkFalsy: true }).isString().isURL().trim(),
+    check('shippingCosts').exists().isFloat({ min: 0 }).toFloat(),
+    check('email').optional({ nullable: true, checkFalsy: true }).isString().isEmail().trim(),
+    check('phone').optional({ nullable: true, checkFalsy: true }).isString().isLength({ min: 1, max: 255 }).trim(),
+    check('restaurantCategoryId').exists({ checkNull: true }).isInt({ min: 1 }).toInt(),
+    check('userId').not().exists(),
+    check('heroImage').custom((value, { req }) => {
+      return checkFileIsImage(req, 'heroImage')
+    }).withMessage('Please upload an image with format (jpeg, png).'),
+    check('heroImage').custom((value, { req }) => {
+      return checkFileMaxSize(req, 'heroImage', maxFileSize)
+    }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+    check('logo').custom((value, { req }) => {
+      return checkFileIsImage(req, 'logo')
+    }).withMessage('Please upload an image with format (jpeg, png).'),
+    check('logo').custom((value, { req }) => {
+      return checkFileMaxSize(req, 'logo', maxFileSize)
+    }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+
+    // solucion
+    check('promoted')
+      .custom(async (value, { req }) => {
+        return checkBusinessRuleOneResturantPromotedByOwner(req.user.id, value)
+      })
+      .withMessage('You can only promote one restaurant at a time')
+
+  ],
+  update: [
+    check('name').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+    check('description').optional({ nullable: true, checkFalsy: true }).isString().trim(),
+    check('address').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+    check('postalCode').exists().isString().isLength({ min: 1, max: 255 }),
+    check('url').optional({ nullable: true, checkFalsy: true }).isString().isURL().trim(),
+    check('shippingCosts').exists().isFloat({ min: 0 }).toFloat(),
+    check('email').optional({ nullable: true, checkFalsy: true }).isString().isEmail().trim(),
+    check('phone').optional({ nullable: true, checkFalsy: true }).isString().isLength({ min: 1, max: 255 }).trim(),
+    check('restaurantCategoryId').exists({ checkNull: true }).isInt({ min: 1 }).toInt(),
+    check('userId').not().exists(),
+    check('heroImage').custom((value, { req }) => {
+      return checkFileIsImage(req, 'heroImage')
+    }).withMessage('Please upload an image with format (jpeg, png).'),
+    check('heroImage').custom((value, { req }) => {
+      return checkFileMaxSize(req, 'heroImage', maxFileSize)
+    }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+    check('logo').custom((value, { req }) => {
+      return checkFileIsImage(req, 'logo')
+    }).withMessage('Please upload an image with format (jpeg, png).'),
+    check('logo').custom((value, { req }) => {
+      return checkFileMaxSize(req, 'logo', maxFileSize)
+    }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+    check('promoted')
+      .custom(async (value, { req }) => {
+        return checkBusinessRuleOneResturantPromotedByOwner(req.user.id, value)
+      })
+      .withMessage('You can only promote one restaurant at a time')
+
+  ]
+}
---
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
@@ -0,0 +1,285 @@
+/* eslint-disable react/prop-types */
+import React, { useContext, useEffect, useState } from 'react'
+import { StyleSheet, FlatList, Pressable, View } from 'react-native'
+
+// solucion
+import { getAll, remove, promote } from '../../api/RestaurantEndpoints'
+import ImageCard from '../../components/ImageCard'
+import TextSemiBold from '../../components/TextSemibold'
+import TextRegular from '../../components/TextRegular'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import * as GlobalStyles from '../../styles/GlobalStyles'
+import { AuthorizationContext } from '../../context/AuthorizationContext'
+import { showMessage } from 'react-native-flash-message'
+import DeleteModal from '../../components/DeleteModal'
+import restaurantLogo from '../../../assets/restaurantLogo.jpeg'
+import ConfirmationModal from '../../components/ConfirmationModal'
+export default function RestaurantsScreen ({ navigation, route }) {
+  const [restaurants, setRestaurants] = useState([])
+  const [restaurantToBeDeleted, setRestaurantToBeDeleted] = useState(null)
+  // solucion
+  const [restaurantToBePromoted, setRestaurantToBePromoted] = useState(null)
+  const { loggedInUser } = useContext(AuthorizationContext)
+
+  useEffect(() => {
+    if (loggedInUser) {
+      fetchRestaurants()
+    } else {
+      setRestaurants(null)
+    }
+  }, [loggedInUser, route])
+
+  const renderRestaurant = ({ item }) => {
+    return (
+      <ImageCard
+        imageUri={item.logo ? { uri: process.env.API_BASE_URL + '/' + item.logo } : restaurantLogo}
+        title={item.name}
+        onPress={() => {
+          navigation.navigate('RestaurantDetailScreen', { id: item.id })
+        }}
+      >
+
+          <TextRegular numberOfLines={2}>{item.description}</TextRegular>
+          {item.averageServiceMinutes !== null &&
+            <TextSemiBold>Avg. service time: <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>{item.averageServiceMinutes} min.</TextSemiBold></TextSemiBold>
+          }
+
+          {/* SOLUTION */}
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }} >
+              <TextSemiBold>Shipping: <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>{item.shippingCosts.toFixed(2)}€</TextSemiBold></TextSemiBold>
+              {item.promoted &&
+                  <TextRegular textStyle={[styles.badge, { color: GlobalStyles.brandPrimary, borderColor: GlobalStyles.brandSuccess }] }>¡En promoción!</TextRegular>
+              }
+          </View>
+           <View style={styles.actionButtonsContainer}>
+          <Pressable
+            onPress={() => navigation.navigate('EditRestaurantScreen', { id: item.id })
+            }
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed
+                  ? GlobalStyles.brandBlueTap
+                  : GlobalStyles.brandBlue
+              },
+              styles.actionButton
+            ]}>
+          <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
+            <MaterialCommunityIcons name='pencil' color={'white'} size={20}/>
+            <TextRegular textStyle={styles.text}>
+              Edit
+            </TextRegular>
+          </View>
+        </Pressable>
+
+        <Pressable
+            onPress={() => { setRestaurantToBeDeleted(item) }}
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed
+                  ? GlobalStyles.brandPrimaryTap
+                  : GlobalStyles.brandPrimary
+              },
+              styles.actionButton
+            ]}>
+          <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
+            <MaterialCommunityIcons name='delete' color={'white'} size={20}/>
+            <TextRegular textStyle={styles.text}>
+              Delete
+            </TextRegular>
+          </View>
+        </Pressable>
+{/* solucion  */}
+        <Pressable
+            onPress={() => { setRestaurantToBePromoted(item) }}
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed
+                  ? GlobalStyles.brandSuccessTap
+                  : GlobalStyles.brandSuccess
+              },
+              styles.actionButton
+            ]}>
+          <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
+            <MaterialCommunityIcons name='alert-octagram' color={'white'} size={20}/>
+            <TextRegular textStyle={styles.text}>
+              Promote
+            </TextRegular>
+          </View>
+        </Pressable>
+        </View>
+      </ImageCard>
+    )
+  }
+
+  const renderEmptyRestaurantsList = () => {
+    return (
+      <TextRegular textStyle={styles.emptyList}>
+        No restaurants were retreived. Are you logged in?
+      </TextRegular>
+    )
+  }
+
+  const renderHeader = () => {
+    return (
+      <>
+      {loggedInUser &&
+      <Pressable
+        onPress={() => navigation.navigate('CreateRestaurantScreen')
+        }
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed
+              ? GlobalStyles.brandGreenTap
+              : GlobalStyles.brandGreen
+          },
+          styles.button
+        ]}>
+        <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
+          <MaterialCommunityIcons name='plus-circle' color={'white'} size={20}/>
+          <TextRegular textStyle={styles.text}>
+            Create restaurant
+          </TextRegular>
+        </View>
+      </Pressable>
+    }
+    </>
+    )
+  }
+  const fetchRestaurants = async () => {
+    try {
+      const fetchedRestaurants = await getAll()
+      setRestaurants(fetchedRestaurants)
+    } catch (error) {
+      showMessage({
+        message: `There was an error while retrieving restaurants. ${error} `,
+        type: 'error',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
+      })
+    }
+  }
+
+  const removeRestaurant = async (restaurant) => {
+    try {
+      await remove(restaurant.id)
+      await fetchRestaurants()
+      setRestaurantToBeDeleted(null)
+      showMessage({
+        message: `Restaurant ${restaurant.name} succesfully removed`,
+        type: 'success',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
+      })
+    } catch (error) {
+      console.log(error)
+      setRestaurantToBeDeleted(null)
+      showMessage({
+        message: `Restaurant ${restaurant.name} could not be removed.`,
+        type: 'error',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
+      })
+    }
+  }
+
+  // solucion
+  const promoteRestaurant = async (restaurant) => {
+    try {
+      await promote(restaurant.id)
+      await fetchRestaurants()
+      setRestaurantToBePromoted(null)
+      showMessage({
+        message: `Restaurant ${restaurant.name} succesfully promoted`,
+        type: 'success',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
+      })
+    } catch (error) {
+      console.log(error)
+      setRestaurantToBePromoted(null)
+      showMessage({
+        message: `Restaurant ${restaurant.name} could not be promoted.`,
+        type: 'error',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
+      })
+    }
+  }
+  return (
+    <>
+    <FlatList
+      style={styles.container}
+      data={restaurants}
+      renderItem={renderRestaurant}
+      keyExtractor={item => item.id.toString()}
+      ListHeaderComponent={renderHeader}
+      ListEmptyComponent={renderEmptyRestaurantsList}
+    />
+    <DeleteModal
+      isVisible={restaurantToBeDeleted !== null}
+      onCancel={() => setRestaurantToBeDeleted(null)}
+      onConfirm={() => removeRestaurant(restaurantToBeDeleted)}>
+        <TextRegular>The products of this restaurant will be deleted as well</TextRegular>
+        <TextRegular>If the restaurant has orders, it cannot be deleted.</TextRegular>
+    </DeleteModal>
+
+    {/* solucion */}
+    <ConfirmationModal
+      isVisible={restaurantToBePromoted !== null}
+      onCancel={() => setRestaurantToBePromoted(null)}
+      onConfirm={() => promoteRestaurant(restaurantToBePromoted)}>
+        <TextRegular>Other promoted restaurant, if any, will be unpromoted</TextRegular>
+
+    </ConfirmationModal>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  button: {
+    borderRadius: 8,
+    height: 40,
+    marginTop: 12,
+    padding: 10,
+    alignSelf: 'center',
+    flexDirection: 'row',
+    width: '80%'
+  },
+  actionButton: {
+    borderRadius: 8,
+    height: 40,
+    marginTop: 12,
+    margin: '1%',
+    padding: 10,
+    alignSelf: 'center',
+    flexDirection: 'column',
+    width: '33%'
+  },
+  actionButtonsContainer: {
+    flexDirection: 'row',
+    bottom: 5,
+    position: 'absolute',
+    width: '90%'
+  },
+  text: {
+    fontSize: 16,
+    color: 'white',
+    alignSelf: 'center',
+    marginLeft: 5
+  },
+  emptyList: {
+    textAlign: 'center',
+    padding: 50
+  },
+
+  // solucion
+  badge: {
+    textAlign: 'center',
+    borderWidth: 2,
+    paddingHorizontal: 10,
+    borderRadius: 10
+  }
+})
